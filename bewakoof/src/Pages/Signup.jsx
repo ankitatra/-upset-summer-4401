b3e80fcaeddfd1ac9855475b1ac64526bc9dc150
Nandan Kumar Singh <nandan3512@gmail.com>
2022-12-17 15:10:21 +0530

    41

---
+++ bewakoof/src/Pages/Signup.jsx
@@ -0,0 +1,120 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  FormControl,
+  HStack,
+  Image,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import { EmailIcon } from "@chakra-ui/icons";
+import { FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { useState } from "react";
+import { Signupd } from "../Comp/Signupd";
+import { redirect } from "react-router-dom";
+
+const Signup = ({mob}) => {
+  const [mobile, setMobile] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();;
+  };
+
+  return (
+    <Flex>
+      <Box w="50%" h="100vh" bgGradient="linear(to-t, yellow.100, white)">
+        <Text fontSize={{ base: "15px", md: "40px", lg: "40px" }} as="b">
+          Welcome to the world of Fashion Store!
+        </Text>
+        <Image
+          m={{ base: "50vh 0 0 0", md: "0", lg: "0" }}
+          src="https://images.bewakoof.com/web/group-19-1617704502.png"
+        />
+      </Box>
+      <Flex direction={"column"} align="center" justifyContent="center" w="50%">
+        <form w="80%" onSubmit={handleSubmit}>
+          <FormControl textAlign="center">
+            <Text fontSize="25px" as="b">
+              Log in / Sign up
+            </Text>
+            <Text color={"grey"}>
+              for Latest trends, exciting offers and everything Fashion Store!
+            </Text>
+            <Input
+              maxLength={'10'}
+              placeholder="Enter Mobile Number"
+              mt={4}
+              w="80%"
+              h={"50px"}
+              fontSize="15px"
+              value={mobile}
+              onChange={(e) => setMobile(e.target.value)}
+            />
+            <Button
+              fontSize="20px"
+              mt={4}
+              w="80%"
+              h={"50px"}
+              colorScheme="teal"
+              type="submit"
+            >
+              CONTINUE
+            </Button>
+          </FormControl>
+        </form>
+
+        <HStack w="80%" mt={4}>
+          <Divider bgColor={"blackAlpha.500"} h="0.5px" />
+          <Text>OR</Text>
+          <Divider bgColor={"blackAlpha.500"} h="0.5px" />
+        </HStack>
+        <Button
+          mt={4}
+          w="63%"
+          h={"40px"}
+          fontSize="15px"
+          variant="outline"
+          leftIcon={<EmailIcon />}
+        >
+          CONTINUE WITH EMAIL
+        </Button>
+        <HStack align={"center"} justifyContent="center" mt={3} w="65%">
+          <Button
+            w="50%"
+            h={"40px"}
+            fontSize="15px"
+            variant="outline"
+            leftIcon={<FcGoogle />}
+          >
+            GOOGLE
+          </Button>
+          <Button
+            w="60%"
+            h={"40px"}
+            fontSize="15px"
+            variant="outline"
+            leftIcon={<FaFacebook color="#3a5996" />}
+          >
+            FACEBOOK
+          </Button>
+        </HStack>
+        <Text mt={3} w="65%" color={"grey"} fontSize="15px">
+          By creating an account or logging in, you agree with Fashion Store's{" "}
+          <Text color={"teal"} as="b" fontSize="15px">
+            Terms and Conditions
+          </Text>{" "}
+          and{" "}
+          <Text color={"teal"} as="b" fontSize="15px">
+            Privacy Policy
+          </Text>
+          .
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export { Signup };
